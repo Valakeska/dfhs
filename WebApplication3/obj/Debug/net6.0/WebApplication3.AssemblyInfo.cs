558f5d8d4d4277f6dab37d44ebe074c51cc2cd2f
RuslanaBelyakova <deep-crusher@mail.ru>
2024-04-07 19:50:15 +0300

    first commit

--- WebApplication3/obj/Debug/net6.0/WebApplication3.AssemblyInfo.cs
+++ WebApplication3/obj/Debug/net6.0/WebApplication3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1502c18f7a2ccf52a0bfeb20c502e6116bcb2456")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38524c233f72da62590ba5ace83975afd410ff8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
